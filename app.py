b27969c29bde9ac1389792043a3186f656c5623d
danielhersh <daniel.hershkovitz.43@gmail.com>
2022-12-14 15:29:21 +0200

    change get demand to accept range of dates instead of one date

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from df_objects import *
 
 d = DemandHourly()
-print(d.get_demand_by_date(datetime.datetime(2017, 1, 1, 13)))
+print(d.get_demand_daily_by_range_of_date(datetime.datetime(2017, 1, 1, 13), datetime.datetime(2017, 1, 3, 13)))
--- df_objects.py
+++ df_objects.py
@@ -10,17 +10,32 @@ class DemandHourly:
     def __init__(self):
         titles = ["Date", "PartOfTheDay", 1, 2, 3,4, 5, 6, 7, 8, 9, 10, 11, 12]
         self.df = pd.read_csv("data/hourlyConsumptionPrediction.csv", names=titles)
+        self.df['Date'] = pd.to_datetime(self.df['Date'], dayfirst=True)
 
-    def get_demand_daily__by_date(self, date: datetime.datetime):
+    def get_demand_daily_by_date(self, date: datetime.datetime):
         """
         return the predicted consumption in given date
         :param date: the requested time
-        :return: arr of the power consumption at that day
+        :return: arr of the power consumption at that day by hour
+        """
+        return self.get_demand_daily_by_range_of_date(date, date + datetime.timedelta(days=1))
+
+    def get_demand_daily_by_range_of_date(self, start_date: datetime.datetime, end_date: datetime.datetime):
+        """
+        return the predicted consumption in given range of date (including start_date excluding end_date)
+        :param end_date: the start date
+        :param start_date: the end date
+        :return: arr of the power consumption at that date range by hour
         """
         # TODO: normalize the data that wil fit the city
-        daily = self.df[(self.df['Date'] == date.strftime("%d/%m/20%y"))].to_numpy()
 
-        return np.append(daily[0][2:], daily[1][2:])
+        # make sure the dates are at the beginning of the day
+        start_date = start_date.replace(hour=0)
+        end_date = end_date.replace(hour=0)
+
+        period = self.df[(self.df['Date'] >= start_date) & (self.df['Date'] < end_date)].to_numpy()
+        hourly_consumption_arr = np.concatenate([day[2:] for day in period])  # remove PartOfTheDay and the date
+        return hourly_consumption_arr
 
 
 class PVProduction:
