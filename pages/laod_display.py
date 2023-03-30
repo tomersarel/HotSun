c171d87dec5edbc542dde2e64118ed796bf610c2
liorsal <lior92626@gmail.com>
2023-03-30 16:33:49 +0300

    contain all the mission i had to the week

--- pages/laod_display.py
+++ pages/laod_display.py
@@ -1,7 +1,7 @@
 from imports import *
 from df_objects import *
-import pandas as pdg
-
+import pandas as pd
+import numpy as np
 
 energy_columns = ['Batteries', 'Solar', 'Buying', 'Selling', 'Lost', 'Storaged']
 colors = {"Solar": "#ffe205", "Batteries": "#d4d4d4", "Buying": "#ec4141", "Selling": "#5fbb4e", "Storaged": "#9edbf9",
@@ -35,63 +35,125 @@ def dict_to_dataframe(df):
     return df
 
 
-def get_display(config, df_energy, df_finance):
+def calculus(config, df_energy):
+    # calculate solar energy percentage
     df_energy['Total'] = df_energy['Solar'] + df_energy['Buying'] + df_energy['Batteries']
-    df_PollutionRates = pdg.read_csv(r"data/PollutionRates.csv")
-    # Calculate solar percentage
-    total_electricity = df_energy['Total'].sum()
-    solar_electricity = df_energy['Solar'].sum()
-    solar_percentage = round((solar_electricity / total_electricity) * 100, 2)
 
-    # Calculate CO2 pollution saved
-    energy_saved = 2200 # replace with desired energy amount
-    co2_saved = round(energy_saved * 0.526, 2) # based on average CO2 emissions per kWh
+    total_energy = df_energy['Total'].sum()
+    solar_energy = df_energy['Solar'].sum()
+    solar_percentage = round((solar_energy / total_energy) * 100, 2)
 
-    # Calculate average PollutionRates
-    avg_pollution_rates = round(df_PollutionRates['Pollution'].mean(), 2)
+    # calculate CO2 pollution saved
+    co2_saved = round((solar_energy * 0.0004), 2)
 
-    display_summary = html.Div([
+    solar_energy_production = df_energy['Solar'].mean()
+    electricity_rate = 0.068  # Replace 0.15 with your electricity rate in dollars per kWh
+    israel_produs_power_solar = 1438000
+    impact = (df_energy['Solar'].mean() / israel_produs_power_solar).__round__(5)
+    savings = (solar_energy_production * electricity_rate).__round__(1)
+    # Calculate the impact increase
+    years_in_period = 10
+    zion = sum((df_energy['Solar'].mean() * (1 + 0.05) ** np.arange(0,
+                                                                    years_in_period)) / israel_produs_power_solar).__round__(
+        5)
+    impact_increase = (zion / impact).__round__(0)
+
+    # create a dictionary with all the calculated values
+    results = {
+        'solar_percentage': solar_percentage,
+        'co2_saved': co2_saved,
+        'savings': savings,
+        'impact': impact,
+        'impact_increase': impact_increase,
+
+    }
+
+    return results
+def grade(calculus_results):
+    green_g = 0
+    if (calculus_results['solar_percentage'] > 10):
+        green_g += 30
+    if (calculus_results['impact'] > 0.0001):
+        green_g += 10
+    if (calculus_results['co2_saved'] > 50):
+        green_g += 20
+    if (calculus_results['savings'] > 200):
+        green_g += 40
+
+    return green_g
+def get_display(config, df_energy, df_finance):
+    results = calculus(config, df_energy)
+    green_g = grade(results)
+    # create display summary
+    display_summery = html.Div([
         html.Div([
             dbc.Card([
                 dbc.CardBody([
-                    html.H1(f"{solar_percentage}%", className="card-title", style={"text-align": "center", "font-size": 48}),
+                    html.H1(f"{results['solar_percentage']}%", className="card-title", style={"text-align": "center", "font-size": 48}),
                     html.H4("Solar Energy", className="card-title", style={"text-align": "center"}),
                     html.P(
-                        "Percentage of total electricity generated from solar energy.",
+                        "Percentage of solar energy out of total energy consumption.",
                         className="card-text",
                     )
-                ]),
+                ])
             ], style={"width": "18rem"}, className="mx-2 my-2"),
             dbc.Card([
                 dbc.CardBody([
-                    html.H1(f"{co2_saved} kg", className="card-title", style={"text-align": "center", "font-size": 48}),
-                    html.H4("Pollution", className="card-title", style={"text-align": "center"}),
+                    html.H1(f"{results['co2_saved']} kg", className="card-title", style={"text-align": "center", "font-size": 48}),
+                    html.H4("Pollution Saved", className="card-title", style={"text-align": "center"}),
                     html.P(
                         "Amount of CO2 pollution saved by producing solar energy.",
                         className="card-text",
-                    ),
-                    html.P(
-                        "Formula: (Energy produced in kWh) x 0.526 kg/kWh (average CO2 emissions per kWh)",
-                        className="card-text",
                     )
-                ]),
+                ])
             ], style={"width": "18rem"}, className="mx-2 my-2"),
             dbc.Card([
                 dbc.CardBody([
-                    html.H1(f"{avg_pollution_rates}", className="card-title", style={"text-align": "center", "font-size": 48}),
-                    html.H4("Pollution Rates", className="card-title", style={"text-align": "center"}),
+                    html.H1(f"{results['savings']} $", className="card-title", style={"text-align": "center", "font-size": 48}),
+                    html.H4("Finance", className="card-title", style={"text-align": "center"}),
                     html.P(
-                        "Average PollutionRates for all rows in the table.",
+                        ('this is how much you have saved by avarage period'),
                         className="card-text",
-                    ),
-                ]),
+                    )
+                ])
             ], style={"width": "18rem"}, className="mx-2 my-2"),
-        ], className="row"),
+            dbc.Card([
+                dbc.CardBody([
+                    html.H1(f"{results['impact']} $", className="card-title", style={"text-align": "center", "font-size": 48}),
+                    html.H4("Climate Change", className="card-title", style={"text-align": "center"}),
+                    html.P(
+                        "this is your procenteg of the israel produs power of solar energy",
+                        className="card-text",
+                    )
+                ])
+            ], style={"width": "18rem"}, className="mx-2 my-2"),
+            dbc.Card([
+                dbc.CardBody([
+                    html.H1(f"{results['impact_increase']} X", className="card-title", style={"text-align": "center", "font-size": 48}),
+                    html.H4("incrise in impact", className="card-title", style={"text-align": "center"}),
+                    html.P(
+                        ('this is how much will be your procenteg out of tatal israel produs power of solar energy will multiplay if you increase by 5% your prous every year '),
+                        className="card-text",
+                    )
+                ])
+            ], style={"width": "18rem"}, className="mx-2 my-2"),
+            dbc.Card([
+                dbc.CardBody([
+                    html.H1(f"{green_g} /100",  className="card-title",
+                            style={"text-align": "center", "font-size": 48}),
+                    html.H4("incrise in impact", className="card-title", style={"text-align": "center"}),
+                    html.P(
+                        (
+                            'this is your green grade '),
+                        className="card-text",
+                    )
+                ])
+            ], style={"width": "18rem"}, className="mx-2 my-2"),
+        ], className="d-flex flex-wrap justify-content-center"),
         dcc.Graph(figure=go.Figure(
             data=generate_year_enr_graph(config['START_YEAR'], config['END_YEAR'], dict_to_dataframe(df_energy), 'Y'),
             layout=go.Layout(barmode='stack', title=f"yearly energy distribution")))
     ])
-
     display_energy = html.Div([dcc.Slider(id="period_slider",
                                           min=config["START_YEAR"],
                                           max=config["END_YEAR"] - 1,
@@ -124,7 +186,7 @@ def get_display(config, df_energy, df_finance):
                                                 layout=go.Layout(barmode='stack', title=f"total pollution per period")),
                                             style={"height": "600px"})])
 
-    display = html.Div([dbc.Accordion([dbc.AccordionItem(display_summary, title='Summery'),
+    display = html.Div([dbc.Accordion([dbc.AccordionItem(display_summery, title='Summery'),
                                        dbc.AccordionItem(display_energy, title='Energy'),
                                        dbc.AccordionItem(display_finance, title='Finance'),
                                        dbc.AccordionItem(display_pollution, title='Pollution')],
@@ -132,7 +194,6 @@ def get_display(config, df_energy, df_finance):
 
                         ], style={"overflow": "auto", "height": "92vh"})
     return display
-
 
 def get_parameters(config):
     result = []
