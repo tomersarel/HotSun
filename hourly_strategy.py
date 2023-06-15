76df3f47812cdb02c990bdda573ccc5d29fa4180
ronelgazar <ronelgazar2017@gmail.com>
2023-06-15 10:22:48 +0300

    made manager more robust for debugging

--- hourly_strategy.py
+++ hourly_strategy.py
@@ -79,27 +79,28 @@ class GreenEnergyUtilizationStrategy(DailyStrategy):
     def __init__(self, buy_rate: float, sell_rate: float):
         self.buy_rate = buy_rate
         self.sell_rate = sell_rate
-
+        self.state = None
     def __call__(self, state: state.State, demand: np.array, solar_rad: np.array):
         batteries = [0] * 24
         solar = [0] * 24
         buying = [0] * 24
         selling = [0] * 24
         storaged = [0] * 24
-        state.batteries.sort(key=lambda battery: battery.efficiency, reverse=True)
+        self.state = state
+        self.state.batteries.sort(key=lambda battery: battery.efficiency, reverse=True)
 
         for hour in range(24):
             hourly_demand = demand[hour]
             solar_production = sum([panel.calc_energy_gen_hourly(solar_rad[hour])
-                                    for panel in state.solar_panels])
+                                    for panel in self.state.solar_panels])
             
             if solar_production > hourly_demand:
                 solar[hour] = hourly_demand
 
                 to_charge = solar_production - hourly_demand
                 battery_index = 0
-                while not math.isclose(to_charge, 0, abs_tol=10 ** -10) and battery_index < len(state.batteries):
-                    charged = state.batteries[battery_index].try_charge(to_charge)
+                while not math.isclose(to_charge, 0, abs_tol=10 ** -10) and battery_index < len(self.state.batteries):
+                    charged = self.state.batteries[battery_index].try_charge(to_charge)
                     storaged[hour] += charged
                     to_charge -= charged
                     battery_index += 1
@@ -109,33 +110,34 @@ class GreenEnergyUtilizationStrategy(DailyStrategy):
                 solar[hour] = solar_production
                 to_supply = hourly_demand - solar_production
                 battery_index = 0
-                while not math.isclose(to_supply, 0, abs_tol=10 ** -10) and battery_index < len(state.batteries):
-                    discharged = state.batteries[battery_index].try_discharge(to_supply)
+                while not math.isclose(to_supply, 0, abs_tol=10 ** -10) and battery_index < len(self.state.batteries):
+                    discharged = self.state.batteries[battery_index].try_discharge(to_supply)
                     to_supply -= discharged
                     batteries[hour] += discharged
                     battery_index += 1
                 buying[hour] = to_supply
-            state.current_date += datetime.timedelta(hours=1)
+            self.state.current_date += datetime.timedelta(hours=1)
 
-        new_batteries_energy = state.batteries[-1].capacity
-        new_panels_power = state.solar_panels[-1].amount * state.solar_panels[-1].max_power
+        new_batteries_energy = self.state.batteries[-1].capacity
+        new_panels_power = self.state.solar_panels[-1].amount * self.state.solar_panels[-1].max_power
 
-        result = pd.DataFrame({'Date': [state.current_date + datetime.timedelta(hours=i) for i in range(24)],
+        result = pd.DataFrame({'Date': [self.state.current_date + datetime.timedelta(hours=i) for i in range(24)],
                               'Batteries': batteries, 'Solar': solar, 'Buying': buying, 'Selling': selling,
                               'Lost': [0] * 24, 'Storaged': storaged,
                               "NewBatteries": [new_batteries_energy] * 24,
-                              "AllBatteries": [len(state.batteries)] * 24,
+                              "AllBatteries": [len(self.state.batteries)] * 24,
                               "NewSolarPanels": [new_panels_power] * 24,
-                              "AllSolarPanels": [len(state.solar_panels)] * 24})
+                              "AllSolarPanels": [len(self.state.solar_panels)] * 24})
 
-        return result, state
+        return result, self.state
 
 
 class Manager(DailyStrategy):
-    # this algorithm returns the best result between the two algorithms passed as parameters as it provides the best of both words for the long run
-    #self.economic_greedy_algorithm is EconomicGreedyStrategy
-    #self.green_energy_utilization_algorithm is GreenEnergyUtilizationStrategy and they are known ahead of time
     def __init__(self, period):
+        if not period:
+            raise ValueError("Period array cannot be empty")
+        if not all(isinstance(p, bool) for p in period):
+            raise TypeError("Period array must contain only boolean values")
         self.sell_rate = 0.9
         self.buy_rate = 1.1
         self.period = period
@@ -144,11 +146,12 @@ class Manager(DailyStrategy):
         self.green_energy_utilization_algorithm = GreenEnergyUtilizationStrategy(buy_rate=self.sell_rate, sell_rate=self.buy_rate)
         self.flag = False
         self.state = None
+
     def getCurrentPeriodLength(self):
-        # period is an array of boolean values which means true - high rate, false - low rate
-        # calculate the length of the current period
-        # if the current period is high rate, return the length of the high rate period
-        # if the current period is low rate, return the length of the low rate period
+        if not self.period:
+            raise ValueError("Period array cannot be empty")
+        if not all(isinstance(p, bool) for p in self.period):
+            raise TypeError("Period array must contain only boolean values")
         period_length = 0
         for i in range(len(self.period)):
             if self.period[i] == True:
@@ -158,50 +161,54 @@ class Manager(DailyStrategy):
         return period_length
     
     def get_stating_preparation_time(self):
-        # return the time needed to prepare for the next period and charge the batteries to full capacity
+        if not self.state:
+            raise ValueError("State parameter cannot be None")
         time_needed = 0
         period_length = self.getCurrentPeriodLength()
-        # go  over the batteries and calculate the time needed to charge them to full capacity
         for battery in self.state.batteries:
             time_needed += battery.get_max_charge()
-        
-        #return the time needed in hours
         return time_needed
-    
 
     def end_of_period(self):
-        #if the current period is high and going into low , sell all the energy in the batteries
-        #if the current period is low and going into high, buy enough energy to fill the batteries
-        #if the current period is high and going into high, do nothing
-        #if the current period is low and going into low, do nothing
+        if not self.period:
+            raise ValueError("Period array cannot be empty")
+        if not all(isinstance(p, bool) for p in self.period):
+            raise TypeError("Period array must contain only boolean values")
         period_length = self.getCurrentPeriodLength()
-        if self.period[period_length - 1] == True and self.period[period_length] == False: # going from high to low 
+        if self.period[period_length - 1] == True and self.period[period_length] == False:
             self.should_sell = True
-        elif self.period[period_length - 1] == False and self.period[period_length] == True: # going from low to high
+        elif self.period[period_length - 1] == False and self.period[period_length] == True:
             self.should_sell = False
         else:   
             self.should_sell = True
 
     def __call__(self, state: state.State, demand: np.array, solar_rad: np.array):
+        if not state:
+            raise ValueError("State parameter cannot be None")
         self.state = state
-        #loop through the period and simulate the algorithm for each hour
-        # if the current hour in the preiod index is the hour to begin charging, charge or sell fully the batteries according to the period
-        # if the current hour is not the hour to begin charging, use the algorithms to calculate the energy to buy or sell
-        # return the best result between the two algorithms passed as parameters as it provides the best of both words for the long run
         result = None
-        for i in range(len(self.period)):
-            if i == self.get_stating_preparation_time():
-                self.end_of_period()
-                self.flag = True
-            if self.flag == True:
-                if self.should_sell == True:
+        try:
+            for i in range(len(self.period)):
+                if i == self.get_stating_preparation_time():
+                    self.end_of_period()
+                    self.flag = True
+                if self.flag == True:
+                    if self.should_sell == True:
+                        result = self.economic_greedy_algorithm(state, demand, solar_rad)
+                    else:
+                        result,final_state = self.green_energy_utilization_algorithm(state, demand, solar_rad)
+                else:
+                    result,final_state = self.economic_greedy_algorithm(state, demand, solar_rad)
+                if result['Buying'].sum() < result['Selling'].sum():
                     result = self.economic_greedy_algorithm(state, demand, solar_rad)
                 else:
                     result,final_state = self.green_energy_utilization_algorithm(state, demand, solar_rad)
-            else:
-                result,final_state = self.economic_greedy_algorithm(state, demand, solar_rad)
-        return result, final_state
-   
+                state = final_state
+        except Exception as e:
+            return {"error": str(e)}, state
+        # align dash.exceptions.LongCallbackError: An error occurred inside a long callback: shapes (864,) and (8760,) not aligned: 864 (dim 0) != 8760 (dim 0)
+
+        return result, self.state
 
 class GreedyDailyStrategy(DailyStrategy):
     def __call__(self, state: state.State, demand: np.array, solar_rad: np.array):
