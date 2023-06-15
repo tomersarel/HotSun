4745036d20412c9cbfe4e9cb4ec3ebd2f077dd58
hillelstern <hillel.stern4@gmail.com>
2023-06-15 11:30:51 +0300

    parameter error

--- error_calc.py
+++ error_calc.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from distutils.command.config import config
 import pandas as pd
 import numpy as np
-import battery
 import hourly_strategy
 import period_strategy
 from df_objects import DemandHourlyCityData, SolarProductionHourlyDataPVGIS, DemandHourlyStateData
@@ -12,142 +11,117 @@ from config_manager import ConfigGetter
 
 demand_hourly = DemandHourlyStateData()
 solar_rad_hourly = SolarProductionHourlyDataPVGIS(ConfigGetter['LOCATION']['longitude'],
-                                                          ConfigGetter['LOCATION']['latitude'],
-                                                          ConfigGetter['solar']['peakpower'],
-                                                          ConfigGetter['solar']['loss'])
+                                                  ConfigGetter['LOCATION']['latitude'],
+                                                  ConfigGetter['solar']['peakpower'],
+                                                  ConfigGetter['solar']['loss'])
 manager = Manager(demand_hourly, [period_strategy.PeriodStrategy(10000, 100) for i in range(33)], [],
-                      solar_rad_hourly,
-                      hourly_strategy.GreedyDailyStrategy(), ConfigGetter)
+                  solar_rad_hourly,
+                  hourly_strategy.GreedyDailyStrategy(), ConfigGetter)
 
 
 def n(a):
     pass
 
 
-'''data = {'a': np.arange(50),
-        'c': np.random.randint(0, 50, 50),
-        'd': np.random.randn(50)}
-data['b'] = data['a'] + 10 * np.random.randn(50)
-data['d'] = np.abs(data['d']) * 100
-
-plt.scatter('a', 'b', c='c', s='d', data=data)
-plt.xlabel('entry a')
-plt.ylabel('entry b')
-plt.show()
-'''
-# rate_set = [1400,1450,1500,1550,1600]
-# arr_r = []
-# for i in rate_set:
-#     manager = Manager(demand_hourly, [period_strategy.PeriodStrategy(10000, 100) for i in range(33)], [],
-#                       solar_rad_hourly,
-#                       hourly_strategy.GreedyDailyStrategy(), ConfigGetter)
-#     batteryChange = ConfigGetter["battery"]
-#     batteryChange["charge_rate"] = i
-#     arr_r.append(manager.run_simulator(a))
-#     print(arr_r[-1])
-# result = arr_r[0].copy(deep=True)
-#
-# array_3d_r = np.stack([df.to_numpy() for df in arr_r])
-#
-# min_values = np.min(array_3d_r, axis=0)
-# max_values = np.max(array_3d_r, axis=0)
-# min_df_r = pd.DataFrame(min_values, columns=arr_r[0].columns)
-# max_df_r = pd.DataFrame(max_values, columns=arr_r[0].columns)
-# min_df_r.to_csv('charge_rate_min_r.csv')
-# max_df_r.to_csv('charge_rate_max_r.csv')
-
-
-'''capacity_set = [4900,4950,5000,5050,5100]
-arr_c = []
-for i in capacity_set:
-    batteryChange = ConfigGetter["battery"]
-    batteryChange["capacity"] = i
-    arr_c.append(manager.run_simulator(a))
-
-array_3d = np.stack([df.to_numpy() for df in arr_c])
-
-min_values_c = np.min(array_3d, axis=0)
-max_values_c = np.max(array_3d, axis=0)
-min_df_c = pd.DataFrame(min_values, columns=arr_c[0].columns)
-max_df_c = pd.DataFrame(max_values, columns=arr_c[0].columns)
-min_df_c.to_csv('charge_rate_min_c.csv')
-max_df_c.to_csv('charge_rate_max_c.csv')
-'''
 d = pd.read_csv("Sumenergy.csv")
 original_df = manager.run_simulator(n)
 original_df.drop('Date', axis='columns', inplace=True)
 original_df.to_csv('original_data')
 
-def calc_parameter_error(name, prercent, amount):
 
-    name_set = np.linspace(ConfigGetter["battery"][name]*((100 - prercent)/100), ConfigGetter["battery"][name]*((100 + prercent)/100), amount)
+def calc_parameter_error(name, percent, amount):
+    # create a numpy array with every parameter value
+    name_set = np.linspace(ConfigGetter["battery"][name] * ((100 - percent) / 100),
+                           ConfigGetter["battery"][name] * ((100 + percent) / 100), amount)
     arr = []
+
+    # running the simulation for each value in name_set
     for i in name_set:
+        # defining the parameter
         ConfigGetter["battery"][name] = i
+        # running the simulation
         tmp = manager.run_simulator(n)
+        # dropping the date column (numpy don't work with '-')
         tmp.drop('Date', axis='columns', inplace=True)
+        # converting the csv from the simulation to numpy (it comes in pandas)
         np_a = tmp.to_numpy()
         arr.append(np_a)
 
     simulation_arr = np.stack(arr)
 
+    # take the minimum of each hour for each parameter
     minimum_values = np.min(simulation_arr, axis=0)
+    # take the maximum of each hour for each parameter
     maximum_values = np.max(simulation_arr, axis=0)
 
+    # the final minimum pandas
     minimum_df = pd.DataFrame(minimum_values, columns=['Batteries', 'Solar', 'Buying',
                                                        'Selling', 'Lost', 'Storaged',
                                                        'NewBatteries', 'AllBatteries',
                                                        'NewSolarPanels', 'AllSolarPanels'])
+    # the final maximum pandas
     maximum_df = pd.DataFrame(maximum_values, columns=['Batteries', 'Solar', 'Buying',
                                                        'Selling', 'Lost', 'Storaged',
                                                        'NewBatteries', 'AllBatteries',
                                                        'NewSolarPanels', 'AllSolarPanels'])
+
+    # converting the normal pandas to numpy
     np_original = original_df.to_numpy()
-
+    # taking the inverse for each box != 0
     reciprocal_values = np.reciprocal(np_original, where=np_original != 0)
-
+    # return to pandas
     pd_df_with_nones = pd.DataFrame(reciprocal_values)
+    # replace nan with 0
     pd_df_with_nones.fillna(0, inplace=True)
+    # back to numpy
     reciprocal_values = pd_df_with_nones.to_numpy()
 
-    total_precent = (maximum_values - minimum_values) * reciprocal_values * 100
-    up_precent = (maximum_values - np_original) * reciprocal_values * 100
-    down_precent = (np_original - minimum_values) * reciprocal_values * 100
+    # calculating the percent of different
+    total_percent = (maximum_values - minimum_values) * reciprocal_values * 100
+    up_percent = (maximum_values - np_original) * reciprocal_values * 100
+    down_percent = (np_original - minimum_values) * reciprocal_values * 100
 
-    total_precent = pd.DataFrame(total_precent)
-    up_precent = pd.DataFrame(up_precent)
-    down_precent = pd.DataFrame(down_precent)
+    # convert to pandas
+    total_percent = pd.DataFrame(total_percent)
+    up_percent = pd.DataFrame(up_percent)
+    down_percent = pd.DataFrame(down_percent)
+
+    # put down to a list
+    frames = [total_percent, up_percent, down_percent]
+
+    # convert to a pandas DataFrame
+    final_percent = pd.concat(frames)
+
+    # return in the order: maximum, minimum, percent
+    return maximum_df, minimum_df, final_percent
 
 
-    frames = [total_precent, up_precent, down_precent]
+csv_of_maximum_for_each_parameter_each_hour, csv_of_minimum_for_each_parameter_each_hour, \
+    csv_of_total_than_upp_than_down_percent_between_the_maximum_and_minimum = calc_parameter_error("capacity", 5, 5)
+csv_of_maximum_for_each_parameter_each_hour.to_csv('maximum_capacity.csv')
+csv_of_minimum_for_each_parameter_each_hour.to_csv('minimum_capacity.csv')
+csv_of_total_than_upp_than_down_percent_between_the_maximum_and_minimum.to_csv('totalprecent_capacity.csv')
 
-    final_precent = pd.concat(frames)
-    return maximum_df, minimum_df, final_precent
+csv_of_maximum_for_each_parameter_each_hour, csv_of_minimum_for_each_parameter_each_hour, \
+    csv_of_total_than_upp_than_down_percent_between_the_maximum_and_minimum = calc_parameter_error("lifetime", 5, 5)
+csv_of_maximum_for_each_parameter_each_hour.to_csv('maximum_lifetime.csv')
+csv_of_minimum_for_each_parameter_each_hour.to_csv('minimum_lifetime.csv')
+csv_of_total_than_upp_than_down_percent_between_the_maximum_and_minimum.to_csv('totalprecent_lifetime.csv')
 
+csv_of_maximum_for_each_parameter_each_hour, csv_of_minimum_for_each_parameter_each_hour, \
+    csv_of_total_than_upp_than_down_percent_between_the_maximum_and_minimum = calc_parameter_error("charge_rate", 5, 5)
+csv_of_maximum_for_each_parameter_each_hour.to_csv('maximum_charge_rate.csv')
+csv_of_minimum_for_each_parameter_each_hour.to_csv('minimum_charge_rate.csv')
+csv_of_total_than_upp_than_down_percent_between_the_maximum_and_minimum.to_csv('totalprecent_charge_rate.csv')
 
-a, b, c = calc_parameter_error("capacity", 5, 5)
-a.to_csv('maximum_capacity.csv')
-b.to_csv('minimum_capacity.csv')
-c.to_csv('totalprecent_capacity.csv')
+csv_of_maximum_for_each_parameter_each_hour, csv_of_minimum_for_each_parameter_each_hour, \
+    csv_of_total_than_upp_than_down_percent_between_the_maximum_and_minimum = calc_parameter_error("efficiency", 5, 5)
+csv_of_maximum_for_each_parameter_each_hour.to_csv('maximum_efficiency.csv')
+csv_of_minimum_for_each_parameter_each_hour.to_csv('minimum_efficiency.csv')
+csv_of_total_than_upp_than_down_percent_between_the_maximum_and_minimum.to_csv('totalprecent_efficiency.csv')
 
-
-a, b, c = calc_parameter_error("lifetime", 5, 5)
-a.to_csv('maximum_lifetime.csv')
-b.to_csv('minimum_lifetime.csv')
-c.to_csv('totalprecent_lifetime.csv')
-
-a, b, c = calc_parameter_error("charge_rate", 5, 5)
-a.to_csv('maximum_charge_rate.csv')
-b.to_csv('minimum_charge_rate.csv')
-c.to_csv('totalprecent_charge_rate.csv')
-
-a, b, c = calc_parameter_error("efficiency", 5, 5)
-a.to_csv('maximum_efficiency.csv')
-b.to_csv('minimum_efficiency.csv')
-c.to_csv('totalprecent_efficiency.csv')
-
-a, b, c = calc_parameter_error("decay_rate", 5, 5)
-a.to_csv('maximum_decay_rate.csv')
-b.to_csv('minimum_decay_rate.csv')
-c.to_csv('totalprecent_decay_rate.csv')
-
+csv_of_maximum_for_each_parameter_each_hour, csv_of_minimum_for_each_parameter_each_hour, \
+    csv_of_total_than_upp_than_down_percent_between_the_maximum_and_minimum = calc_parameter_error("decay_rate", 5, 5)
+csv_of_maximum_for_each_parameter_each_hour.to_csv('maximum_decay_rate.csv')
+csv_of_minimum_for_each_parameter_each_hour.to_csv('minimum_decay_rate.csv')
+csv_of_total_than_upp_than_down_percent_between_the_maximum_and_minimum.to_csv('totalprecent_decay_rate.csv')
--- graphs_of_errors.py
+++ graphs_of_errors.py
@@ -27,10 +27,51 @@ mini = pd.read_csv('minimum_lifetime.csv')
 plt.plot([i for i in maxi.index], maxi['Batteries'] - mini['Batteries'])
 plt.ylabel('some numbers')
 '''
+orig = pd.read_csv('original_data')
 
-data = pd.read_csv('totalprecent_capacity.csv')
-lst = [i for i in data.index]
-plt.plot([i for i in data.index], data['1'],)
+def calculate_percent(maxi, mini):
+    # converting the normal pandas to numpy
+    np_orig = orig.to_numpy()
+    # taking the inverse for each box != 0
+    reciprocal_values = np.reciprocal(np_orig, where=np_orig != 0)
+    # return to pandas
+    pd_df_with_nones = pd.DataFrame(reciprocal_values)
+    # replace nan with 0
+    pd_df_with_nones.fillna(0, inplace=True)
+    # back to numpy
+    reciprocal_values = pd_df_with_nones.to_numpy()
+
+    upercent = (maxi - orig) * reciprocal_values * 100
+    downpercent = (orig - mini) * reciprocal_values * 100
+    totpercent = upercent + downpercent
+
+    total_percent = pd.DataFrame(totpercent)
+    up_percent = pd.DataFrame(upercent)
+    down_percent = pd.DataFrame(downpercent)
+
+    # frames = [total_percent, up_percent, down_percent]
+
+    # final_percent = pd.concat(frames)
+
+    return total_percent
+
+
+np_orig = orig.to_numpy()
+# taking the inverse for each box != 0
+reciprocal_values = np.reciprocal(np_orig, where=np_orig != 0)
+# return to pandas
+pd_df_with_nones = pd.DataFrame(reciprocal_values)
+# replace nan with 0
+pd_df_with_nones.fillna(0, inplace=True)
+# back to numpy
+inverse = pd_df_with_nones.to_numpy()
+
+
+
+maximum = pd.read_csv('maximum_capacity.csv')
+minimum = pd.read_csv('minimum_capacity.csv')
+data = calculate_percent(maximum, minimum)
+plt.plot(data['Solar'])
 plt.ylabel('some numbers')
 plt.show()
 # [0:289079]
